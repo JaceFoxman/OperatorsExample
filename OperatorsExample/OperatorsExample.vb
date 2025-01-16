c7d0ccacec0e9225c48bb5e11fc833ad982eb0b0
Jason Permann <JaceFoxman@gmail.com>
2025-01-16 08:52:21 -0700

    Added header, and 5+3=8 math.

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -1,7 +1,14 @@
-﻿Module OperatorsExample
+﻿'Jason Permann
+'Spring 2025
+'RCET2265
+'OperatorsExample
+'https://github.com/JaceFoxman/OperatorsExample.git
+
+Module OperatorsExample
 
     Sub Main()
-
+        Console.Write("5 + 3 = ") 'print a string
+        Console.WriteLine(5 + 3) 'print the sum
     End Sub
 
 End Module
