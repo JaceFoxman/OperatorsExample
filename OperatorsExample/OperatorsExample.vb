afbbf3f22c595508eb31341bf0df78984f72ea68
Jason Permann <JaceFoxman@gmail.com>
2025-01-17 08:18:59 -0700

    Add more mathmatical operations

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -7,8 +7,25 @@
 Module OperatorsExample
 
     Sub Main()
+        'sum operator 
         Console.Write("5 + 3 = ") 'print a string
         Console.WriteLine(5 + 3) 'print the sum
+        Console.WriteLine()
+
+        'difference Operator
+        Console.Write("5 - 3 = ") 'print a string
+        Console.WriteLine(5 - 3) 'print the sum
+        Console.WriteLine()
+
+        'product opperator
+        Console.Write("5 * 3 = ") 'print a string
+        Console.WriteLine(5 * 3) 'print the sum
+        Console.WriteLine()
+
+        'quotient operator
+        Console.Write("5 / 3 = ") 'print a string
+        Console.WriteLine(5 / 3) 'print the sum
+        Console.WriteLine()
     End Sub
 
 End Module
